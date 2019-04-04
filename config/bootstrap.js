d6382538c4bd2ad83b75baa2c6be953c82614619
tulare <tulare.paxgalactica@gmail.com>
2019-04-04 17:16:27 +0200

    First commit

---
+++ config/bootstrap.js
@@ -0,0 +1,16 @@
+/**
+ * Seed Function
+ * (sails.config.bootstrap)
+ *
+ * A function that runs just before your Sails app gets lifted.
+ * > Need more flexibility?  You can also create a hook.
+ *
+ * For more information on seeding your app with fake data, check out:
+ * https://sailsjs.com/config/bootstrap
+ */
+
+module.exports.bootstrap = function() {
+
+  // By convention, this is a good place to set up fake data during development.
+
+};
